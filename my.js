294fe918221465bd1b8ef4732e20238c642bae00
aiftt <ftt.loves@gmail.com>
2023-11-21 16:43:01 +0800

    2023-11-21 16:43:01

---
+++ my.js
@@ -0,0 +1,13 @@
+window.onload = function () {
+  $('.src').each(function () {
+    const el = $(this).get(0)
+    var lang =
+      $el.className.trim()
+      .replace(/^src\s+src-/, '')
+    $(this).addClass('language-' + lang)
+
+    var text = $(el).text()
+    $(el).html('<code>' + text + '</code>')
+  })
+  hljs.highlightAll()
+}
